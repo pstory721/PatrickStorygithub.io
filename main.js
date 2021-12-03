e2491725a540b7192d6ca111a8de1a2029b83d7b
patrick story <pstory721@gmail.com>
2021-12-03 17:32:05 -0600

    maby

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import './style.css';
 import * as THREE from "./node_modules/three/build/three.module.js"
 
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-
 // Setup
 
 const scene = new THREE.Scene();
